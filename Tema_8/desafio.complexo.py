72c08294f9613500133ae137974b75008ba29843
Poveda <julianopoveda@github.com>
2019-12-25 23:06:14 -0300

    Resposta do desafio.

---
+++ Tema_8/desafio.complexo.py
@@ -0,0 +1,70 @@
+#-*- coding: utf-8 -*-
+valor_emprestimo = 5000.00
+quantidade_parcelas = 60
+idade_pessoa = 23
+nome_cliente ="Fulano de Tal"
+
+
+def PessoaAptaParcelamento(idade_pessoa, quantidade_parcelas) :
+    quantidade_maxima_parcela_geral = 60
+    quantidade_maxima_parcela_idoso = 36
+    
+    #primeiro efetua-se a verificação mais restritiva.
+    if(idade_pessoa > 74 and quantidade_parcelas>quantidade_maxima_parcela_idoso):
+        print ("Desculpe, porém não é possível fazer um parcelamento superior a %dx para pessoas na sua faixa etária" % quantidade_maxima_parcela_idoso)
+        return False
+    #Na segunda condição não é necessário verificar a idade, pois existe restrição somente para idosos.
+    elif quantidade_parcelas>quantidade_maxima_parcela_geral:
+        print ("Desculpe, porém não é possível fazer um parcelamento superior a %dx" % quantidade_maxima_parcela_geral)        
+        return False
+    else:
+        return True
+
+def ObterTaxaJuros(idade_pessoa):
+    taxa_juros = 0
+
+    #Essas variáveis servem para tornar o código mais legível e evitar muitos comentários de explicação
+    faixa_45 = 45
+    faixa_74 = 74
+    faixa_75_mais = 75
+    taxa_juros_ate_45_anos = 0.01
+    taxa_juros_de_46_ate_74_anos = 0.02
+    taxa_juros_a_partir_75_anos = 0.03
+    
+    #Aqui a variável de taxa_juros é iniciada. 
+    #Não é interessante de realizar essa verificação dentro do laço, pois o valor da taxa não depende de nenhum componente do laço
+    if(idade_pessoa <= faixa_45 ):
+        taxa_juros = taxa_juros_ate_45_anos
+    elif(idade_pessoa>faixa_45 and idade_pessoa <=faixa_74):
+        taxa_juros = taxa_juros_de_46_ate_74_anos
+    else:
+        taxa_juros = taxa_juros_a_partir_75_anos
+    
+    return taxa_juros
+
+def CalcularEmprestimo(valor_emprestimo, quantidade_parcelas, idade_pessoa):    
+    valor_parcela = valor_emprestimo/quantidade_parcelas
+    taxa_juros = ObterTaxaJuros(idade_pessoa)
+        
+    mes_corrente = 1
+    valor_parcela_corrigido = 0
+    valor_total = 0    
+
+    #é possível remover o while, deste método. Isso fica como exercicío. Dê uma pesquisada sobre soma de termos em uma PA
+    while(mes_corrente <= quantidade_parcelas):
+        adendo_de_parcela = mes_corrente / 2
+        valor_parcela_corrigido = valor_parcela * ( 1 + taxa_juros )  + adendo_de_parcela
+        valor_total += valor_parcela_corrigido
+        mes_corrente += 1 #esta expressão equivale a: mes_corrente = mes_corrente + 1
+    
+    return valor_total, valor_parcela_corrigido #esta é a forma de retornar mais de um valor da função.
+
+def GerarRelatorio(nome_cliente, valor_emprestimo, quantidade_parcelas, valor_ultima_parcela_corrigida, valor_total_emprestimo):
+    #o caracter \n simboliza quebra de linha, desta forma não é preciso colocar um print para cada linha impressa
+    print("Sr(a): %s \nValor de empréstimo solicitado: %.2f \nQuantidade de Parcelas: %d \nValor da última parcela: %.2f \nValor Total do Empréstimo:%.2f" % (nome_cliente, valor_emprestimo, quantidade_parcelas, valor_ultima_parcela_corrigida, valor_total_emprestimo))
+
+if PessoaAptaParcelamento(idade_pessoa, quantidade_parcelas):
+    valor_total_emprestimo, valor_ultima_parcela_corrigida = CalcularEmprestimo(valor_emprestimo, quantidade_parcelas, idade_pessoa)
+    GerarRelatorio(nome_cliente, valor_emprestimo, quantidade_parcelas, valor_ultima_parcela_corrigida, valor_total_emprestimo)
+else:
+    print("Lamentamos não poder realizar seu empréstimo")
